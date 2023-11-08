d3dc2c4fdd7593161787a866a9cc1821bc8a0d23
HeesuKim0203 <heesukim998@gmail.com>
2023-11-08 20:51:12 +0900

    readme update

---
+++ callJavaScriptFunctionInC/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+#include <emscripten.h>
+#include <emscripten/bind.h>
+
+EM_JS(void, call_js, (
+    const char *subject, int len_subject,
+    const char *msg, int len_msg
+), {
+    jsFunction( 
+        UTF8ToString(subject, len_subject),
+        UTF8ToString(msg, len_msg)
+    )
+}) ;
+
+bool my_CallJs()
+{
+    const std::string subject = "Hellow JS !" ;
+    const std::string msg = "I love JS!" ;
+    
+    call_js(subject.c_str(), subject.length(),
+        msg.c_str(), msg.length() 
+    ) ;
+
+    return true ;
+}
+
+EMSCRIPTEN_BINDINGS(module)
+{
+    emscripten::function("callJs", &my_CallJs) ;
+}
